c7030ca0f559db8e255219aac24be682c9d7a6e0
Carlos J. Ramirez <carlosjramirez11@gmail.com>
2025-02-04 12:29:10 -0800

    Removed files that were not needed and added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <h1 className={styles.navbar__heading}>E-Commerce</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
