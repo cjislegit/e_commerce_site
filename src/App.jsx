ab6dfa970f45d54de110bc31d5409891262de296
cjislegit <carlosjramirez11@gmail.com>
2025-02-06 10:43:03 -0800

    Added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
 import Footer from './components/Footer';
-import Hero from './components/Hero';
 import Navbar from './components/Navbar';
-import Products from './components/Products';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
       <Navbar />
-      <Hero />
-      <Products />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
       <Footer />
     </>
   );
