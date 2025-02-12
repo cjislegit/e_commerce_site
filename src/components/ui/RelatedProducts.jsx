35200eeb298e6d6215cc0109256d7dd9cdf4aa43
Carlos J. Ramirez <carlosjramirez11@gmail.com>
2025-02-12 11:30:23 -0800

    Added container inside related

--- src/components/ui/RelatedProducts.jsx
+++ src/components/ui/RelatedProducts.jsx
@@ -1,7 +1,11 @@
 import styles from './RelatedProducts.module.css';
 
 const RelatedProducts = () => {
-  return <section className={styles.related}>Related</section>;
+  return (
+    <section className={styles.related}>
+      <div className={styles.related__container}>Test</div>
+    </section>
+  );
 };
 
 export default RelatedProducts;
