f456e5c247f9a2cc087d79ac5c1d18c3eaffc50c
Carlos J. Ramirez <carlosjramirez11@gmail.com>
2025-02-12 11:28:04 -0800

    Added max width to related section

--- src/components/ui/RelatedProducts.jsx
+++ src/components/ui/RelatedProducts.jsx
@@ -1,5 +1,7 @@
+import styles from './RelatedProducts.module.css';
+
 const RelatedProducts = () => {
-  return <h1>Related</h1>;
+  return <section className={styles.related}>Related</section>;
 };
 
 export default RelatedProducts;
