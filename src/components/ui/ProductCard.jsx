9e9cb479962ddb6f14e89f9b2710e60f3331bae8
cjislegit <carlosjramirez11@gmail.com>
2025-02-13 20:37:41 -0800

    Made all the titles of the poducts be less than 22 characters

--- src/components/ui/ProductCard.jsx
+++ src/components/ui/ProductCard.jsx
@@ -14,7 +14,10 @@ const ProductCard = ({ product }) => {
         <img src={product.image} alt={product.title} />
       </div>
       <div className={styles.card__data}>
-        <h3 className={styles.card__title}>{product.title}</h3>
+        <h3 className={styles.card__title}>
+          {product.title.slice(0, 22)}
+          {product.title.length > 22 ? '. . .' : null}
+        </h3>
         <p className={styles.card__price}>
           $
           {new Intl.NumberFormat('en-US', { minimumFractionDigits: 2 }).format(
