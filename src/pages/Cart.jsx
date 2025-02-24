b88f8cf3496cddd99ef661302ed156e3c85351fd
Carlos J. Ramirez <carlosjramirez11@gmail.com>
2025-02-23 17:34:39 -0800

    Started working on cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,28 @@
+import styles from './Cart.module.css';
+import Hero from '../components/ui/Hero';
+
 const Cart = () => {
   return (
-    <div>
-      <h1>Cart</h1>
-    </div>
+    <>
+      <Hero headline='Shopping Cart' />
+      <main className={styles.cart}>
+        <section className={styles.cart__container}>
+          <div className={styles.cart__products}>
+            <div className={styles.cart__productTitles}>
+              <h4>Product</h4>
+              <h4>Price</h4>
+              <h4>Quantity</h4>
+              <h4>Total</h4>
+            </div>
+          </div>
+          <div className={styles.cart__total}>
+            <div className={styles.cart__totalTitle}>
+              <h4>Cart Total</h4>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
   );
 };
 
