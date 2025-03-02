4c8004ab462feb7c4b8da5279ec0578182327cce
Carlos J. Ramirez <carlosjramirez11@gmail.com>
2025-03-02 15:14:28 -0800

    Added padding for the cart items

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -24,6 +24,47 @@ const Cart = () => {
     decreaseQuantity(pid);
   };
 
+  const cartItems =
+    cart.length === 0 ? (
+      <h4 className={styles.cart__empty}>Your cart is empty</h4>
+    ) : (
+      cart.map((item) => (
+        <div key={item.id} className={styles.cart__productItem}>
+          <div className={styles.cart__imgContainer}>
+            <div
+              className={styles.cart__imgClose}
+              onClick={() => handleDelete(item.id)}
+            >
+              x
+            </div>
+            <img
+              className={styles.cart__img}
+              src={item.image}
+              alt={item.title}
+            />
+          </div>
+          <span className={styles.cart__title}>{item.title}</span>
+          <span className={styles.cart__price}>
+            {new Intl.NumberFormat('en-US', {
+              minimumFractionDigits: 2,
+            }).format(item.price)}
+          </span>
+          <div className={styles.cart__quantityContainer}>
+            <button onClick={() => handleDecrease(item.id)}>-</button>
+            <span className={styles.cart__quantity}>{item.quantity}</span>
+
+            <button onClick={() => handleIncrease(item.id)}>+</button>
+          </div>
+
+          <span className={styles.cart__itemTotal}>
+            {new Intl.NumberFormat('en-US', {
+              minimumFractionDigits: 2,
+            }).format(item.price * item.quantity)}
+          </span>
+        </div>
+      ))
+    );
+
   return (
     <>
       <Hero headline='Shopping Cart' />
@@ -36,45 +77,7 @@ const Cart = () => {
               <h4 className={styles.cart__quantityTitle}>Quantity</h4>
               <h4 className={styles.cart__totalTitle}>Total</h4>
             </div>
-            <div className={styles.cart__productItems}>
-              {cart.map((item) => (
-                <div key={item.id} className={styles.cart__productItem}>
-                  <div className={styles.cart__imgContainer}>
-                    <div
-                      className={styles.cart__imgClose}
-                      onClick={() => handleDelete(item.id)}
-                    >
-                      x
-                    </div>
-                    <img
-                      className={styles.cart__img}
-                      src={item.image}
-                      alt={item.title}
-                    />
-                  </div>
-                  <span className={styles.cart__title}>{item.title}</span>
-                  <span className={styles.cart__price}>
-                    {new Intl.NumberFormat('en-US', {
-                      minimumFractionDigits: 2,
-                    }).format(item.price)}
-                  </span>
-                  <div className={styles.cart__quantityContainer}>
-                    <button onClick={() => handleDecrease(item.id)}>-</button>
-                    <span className={styles.cart__quantity}>
-                      {item.quantity}
-                    </span>
-
-                    <button onClick={() => handleIncrease(item.id)}>+</button>
-                  </div>
-
-                  <span className={styles.cart__itemTotal}>
-                    {new Intl.NumberFormat('en-US', {
-                      minimumFractionDigits: 2,
-                    }).format(item.price * item.quantity)}
-                  </span>
-                </div>
-              ))}
-            </div>
+            <div className={styles.cart__productItems}>{cartItems}</div>
           </div>
           <div className={styles.cart__total}>
             <div className={styles.cart__totalTitle}>
